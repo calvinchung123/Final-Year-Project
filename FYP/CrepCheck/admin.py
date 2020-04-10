0392811716758692ffe5531d6800bdea7c731074
calvinchung123 <C15529113@mydit.ie>
2020-04-11 00:31:20 +0100

    Final Year
    All code

---
+++ FYP/CrepCheck/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import CrepCheck
+
+
+admin.site.register(CrepCheck)
---
+++ FYP/CrepCheck/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrepcheckConfig(AppConfig):
+    name = 'CrepCheck'
